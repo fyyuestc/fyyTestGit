a789bb60eae22b08d38827546796c0e4ca56f5d4
范云翼 <fanyunyimac@fanyunyimacdemac.local>
2018-09-05 15:21:12 +0800

    Initial Commit

---
+++ fyyTestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fyyTestGit
+//
+//  Created by fanyunyimac on 2018/9/5.
+//  Copyright © 2018年 范云翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
